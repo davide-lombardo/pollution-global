0822c4f26d2449e0fce054d6d09ae2f199a19582
davide-lombardo <davide.lombardo.92@gmail.com>
2021-07-09 20:32:23 +0200

    geolocation btn fix

--- src/index_dev.js
+++ src/index_dev.js
@@ -1,4 +1,6 @@
 import './styles/scss/index.scss';
+import L from 'leaflet';
+import _ from 'lodash';
 import axios from "axios";
 
 
@@ -9,21 +11,39 @@ heading.textContent = "Global Air Pollution";
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-const latitude = document.createElement('span');
-latitude.setAttribute('class', 'lat-input');
+const geoBtn = document.createElement('button');
+geoBtn.innerText = 'Your Position';
+geoBtn.setAttribute('class', 'geo-btn');
 
-const longitude = document.createElement('span');
-longitude.setAttribute('class', 'lon-input');
+// Create a form dynamically
+const form = document.createElement("form");
+form.setAttribute("method", "get");
+form.setAttribute("action", "submit");
+form.setAttribute("id", "form");
 
+// Create an input element for City
+const cityInput = document.createElement("input");
+cityInput.setAttribute("type", "text");
+cityInput.setAttribute('id', 'city-input');
+cityInput.setAttribute("placeholder", "Enter city");
+
+// create a submit button
+const sendBtn = document.createElement("input");
+sendBtn.setAttribute("type", "submit");
+sendBtn.setAttribute("value", "submit");
+sendBtn.setAttribute('class', 'send-btn');
 
 app.append(heading);
 app.append(container);
-container.append('latitude');
-container.append('longitude');
+form.append(cityInput);
+form.append(sendBtn);
+container.append(geoBtn);
+document.getElementsByTagName("body")[0].appendChild(form);
 
 
-//Leaflet map
-const mymap = L.map('mapid').setView([0, 0], 1);
+//create a map with Leaflet
+const mymap = L.map('mapid').setView([0, 0], 2);
+const marker = L.marker([20.7, 90.5]).addTo(mymap);
 
 const attribution = '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -33,27 +53,82 @@ tiles.addTo(mymap);
 
 
 
-//API call
-// (async function requestPollutionData() {
-
-//     const API_KEY = process.env.API_KEY
-
-//     const response = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/token=${API_KEY}`)
-//     const data = await response.json()
-
-//     console.log(data)
-// })();
 
 
+//Get User location
+const succesCallback = async(position) => {
+
+    const { latitude, longitude } = position.coords;
+
+    try {
+
+        const response = await axios.get('https://api.waqi.info/feed/geo:${latitude};${latitude}/?token=${process.env.API_KEY}');
+        console.log(response.data);
+
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+
+    //Leaflet marker gets lat and lon data
+    marker.setLatLng([latitude, longitude]).addTo(mymap);
+};
+
+
+const errorCallback = (error) => {
+
+    console.error(error)
+
+};
+
+
+const getUserLocation = () => {
+
+    navigator.geolocation.getCurrentPosition(succesCallback, errorCallback);
+
+}
 
 
 
-(async() => {
+
+
+
+
+
+// const responseGetLoadash = _.get(responseData, 'data.data.city.name', 'not found')
+// console.log(responseGetLoadash)
+
+// if (responseGetLoadash == error) {
+//     aquiResult = document.getElementById('h2').innerHTML = error
+
+// } else {
+//     resultPollution(resposeData);
+// }
+
+
+
+
+//Get data from API call
+const requestPollution = async() => {
 
     const API_KEY = process.env.API_KEY
 
-    const response = await axios.get(`https://api.waqi.info/feed/geo:${lat};${lon}/token=${process.env.API_KEY}`)
-    const data = await response.data
+    try {
+        const response = await axios.get('https://api.waqi.info/search/?keyword=${keyword}&token=${process.env.API_KEY}');
+        console.log(response.data);
+        // const responseGetLoadash = _.get(response, 'data.data.city.name', 'not found')
+        // console.log(responseGetLoadash);
 
-    console.log(data)
-})();
+    } catch (err) {
+
+        console.error(err);
+    }
+};
+
+
+
+cityInput.addEventListener('submit', requestPollution);
+sendBtn.addEventListener('click', requestPollution);
+geoBtn.addEventListener('click', getUserLocation);
