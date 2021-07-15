7fd5845543d173e846ac72f23676a4265c1d96e7
davide-lombardo <davide.lombardo.92@gmail.com>
2021-07-15 22:54:46 +0200

    netlify functions update

---
+++ netlify/functions/getPollution.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+exports.handler = async function(event, context) {
+    const API_KEY = process.env.API_KEY;
+    const city = event.queryStringParameters.city;
+    const response = await axios.get(`https://api.waqi.info/search/?token=${API_KEY}&keyword=${city}`);
+    const cityNameData = response.data.data[0].station.name;
+    const cityAqi = response.data.data[0].aqi;
+    const data = {
+        name: cityNameData,
+        aqi: cityAqi
+    };
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    };
+}
