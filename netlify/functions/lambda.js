65b78cc7a8fb80937f168caab491542b52dff361
davide-lombardo <davide.lombardo.92@gmail.com>
2021-07-15 21:24:41 +0200

    lambda fix

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,18 +1,36 @@
-exports.handler = async event => {
+const axios = require('axios');
+const qs = require('qs');
 
-    const API_KEY = process.env.API_KEY
+exports.handler = async function(event, context, callBack) {
+
+    const API_PARAMS = qs.stringify(event.queryStringParameters)
+    const { API_KEY, API_URL } = process.env
+    const URL = `${API_URL}?${API_PARAMS}&key${API_KEY}`
 
 
-    const response = await fetch(`endpoint/parameters&API_KEY=${API_KEY}`)
-    const data = await response.json()
+    // Let's log some stuff we already have.
+    console.log("Injecting token to", API_URL);
+    console.log("logging event.....", event)
+    console.log("Constructed URL is ...", URL)
 
-
+    // Here's a function we'll use to define how our response will look like when we call callback
     const pass = (body) => {
-        return {
+        callback(null, {
             statusCode: 200,
             body: JSON.stringify(body)
-        }
+        })
     }
 
-    return pass(data)
-}
+    // Perform the API call.
+    const get = () => {
+        axios.get(URL)
+            .then((response) => {
+                console.log(response.data)
+                pass(response.data)
+            })
+            .catch(err => pass(err))
+    }
+    if (event.httpMethod == 'GET') {
+        get()
+    };
+};
